ccbc80fbaf654987c3b687ab037b0b20cb4441aa
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-06-30 11:08:31 +0200

    remove unused function

--- backend/shared/paths.py
+++ backend/shared/paths.py
@@ -10,21 +10,10 @@ build_path: Path = Path(os.path.dirname(__file__)).parent.parent / "frontend" / 
 
 storage_path: Path = output_path / "storage"
 
-persistence_path: Path = output_path / "persistence"
-
 # Usage: session_path(_SESSION_ID_)
 session_path: Callable[[str], Path] = lambda s: storage_path / f"s_{s}"
 
-# Usage: project_path(_SESSION_ID_, _PROJECT_ID_)
-project_path: Callable[[str, str], Path] = (
-    lambda s, p: storage_path / f"s_{s}" / f"p_{p}"
-)
-
-# Usage: goals_path(_SESSION_ID_, _PROJECT_ID_)
-goals_path: Callable[[str, str], Path] = (
-    lambda s, p: storage_path / f"s_{s}" / f"p_{p}" / "goals"
-)
-
+# Usage: controller_path(_SESSION_ID_)
 controller_path: Callable[str, Path] = (
     lambda s: storage_path / f"s_{s}" / "controllers"
 )
